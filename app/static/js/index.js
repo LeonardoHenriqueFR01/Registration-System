6d5b93e1cd93bd9fa711a1c4be1c779fdee1b51a
Leozindev_genius <leonardohenriquefr785@gmail.com>
2025-03-11 16:36:20 -0300

    interface login commit

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,69 +1,80 @@
+// Função para alternar a visibilidade da senha em um campo de entrada
+function togglePassword(inputId, icon) {
+    // Obtém o elemento de input pelo ID fornecido
+    const input = document.getElementById(inputId);
+    
+    // Verifica se o tipo do input é "password"
+    if (input.type === "password") {
+        // Altera o tipo para "text" para exibir a senha
+        input.type = "text";
+        icon.innerText = "Visibility";
+    } else {
+        // Caso contrário, altera o tipo de volta para "password" para ocultar a senha
+        input.type = "password";
+        icon.innerText = "Visibility_off";
+    }
+}
+
 // Função para troca de formulários
 function toggleForms() {
     // Seleciona o container principal e alterna a classe "active"
     document.getElementById('content').classList.toggle('active');
 }
 
-// Função para chmamar o spineer form cadastro
-function spineer_cadastro() {
-    // Pegando as variáveis do form
+// Função para chamar spin form cadastro
+function spin_cadastro() {
     let name = document.getElementById('name').value.trim()
     let email = document.getElementById('email').value.trim()
     let password = document.getElementById('password').value.trim()
 
-    // Pegando o spineer e o button
-    let load = document.getElementById('load_cadastro')
-    let btn_cadastro = document.getElementById('btn_cadastro')
+    let btn_cadastro = document.getElementById('btn_cadastro')   
+    let load_cadastro = document.getElementById('load_cadastro')
 
-    if (name.length < 3) {
-        alert('O nome deve ter no mínimmo 3 caracteres!')
+    if (name < 3) {
+        alert('O nome deve ter no mínimo 3 caracteres!')
         return;
     }
 
-    if (email.length < 12) {
+    if (email < 12) {
         alert('O email deve ter no mínimo 12 caracteres!')
         return;
     }
 
-    if (password.length < 8) {
+    if (password < 8) {
         alert('A senha deve ter no mínimo 8 caracteres!')
         return;
     }
 
-    // Caso nenhuma das opções acima aconteça
+    // Caso nenhuma das opções a cima ocorra
     btn_cadastro.style.display = "none"
-    load.style.display = "flex"
-
+    load_cadastro.style.display = "block"
 }
 
-// Função para chmamar o spineer form login
-function spineer_login() {
-    // Pegando as variáveis do form
+// Função para chamar spin form login
+function spin_login() {
     let name = document.getElementById('name_login').value.trim()
     let email = document.getElementById('email_login').value.trim()
     let password = document.getElementById('password_login').value.trim()
 
-    // Pegando o spineer e o button
-    let load = document.getElementById('load_login')
-    let btn_login = document.getElementById('btn_login')
+    let btn_login = document.getElementById('btn_login')   
+    let load_login = document.getElementById('load_login')
 
-    if (name.length < 3) {
-        alert('O nome deve ter no mínimmo 3 caracteres!')
+    if (name < 3) {
+        alert('O nome deve ter no mínimo 3 caracteres!')
         return;
     }
 
-    if (email.length < 12) {
+    if (email < 12) {
         alert('O email deve ter no mínimo 12 caracteres!')
         return;
     }
 
-    if (password.length < 8) {
+    if (password < 8) {
         alert('A senha deve ter no mínimo 8 caracteres!')
         return;
     }
 
-    // Caso nenhuma das opções acima aconteça
+    // Caso nenhuma das opções a cima ocorra
     btn_login.style.display = "none"
-    load.style.display = "flex"
-
+    load_login.style.display = "block"
 }
